2da87ac09b52a3e2dd9dc949cace05bf9d3758df
alyashzwan14 <34183187+alyashzwan14@users.noreply.github.com>
2024-07-14 11:37:47 +0800

    Update run.py

--- run.py
+++ run.py
@@ -369,7 +369,7 @@ def CalculateTrade(update: Update, context: CallbackContext) -> int:
 
             # sets the user context trade equal to the parsed trade
             context.user_data['trade'] = trade
-            update.effective_message.reply_text("Trade Successfully Parsed! 🥳\nConnecting to MetaTrader ... (May take a while) ⏰")
+            update.effective_message.reply_text("Trade Successfully Parsed!\nConnecting to MetaTrader ... (May take a while)")
         
         except Exception as error:
             logger.error(f'Error: {error}')
@@ -412,7 +412,7 @@ def welcome(update: Update, context: CallbackContext) -> None:
         context: CallbackContext object that stores commonly used objects in handler callbacks
     """
 
-    welcome_message = "Hi, Welcome to the Telegram Bot! \n\nYou can use this bot to enter trades directly from Telegram and get a detailed look at your risk to reward ratio with profit, loss, and calculated lot size. You are able to change specific settings such as allowed symbols, risk factor, and more from your personalized Python script and environment variables.\n\nUse the /help command to view instructions and example trades."
+    welcome_message = "Hi, Welcome to the ProjexFX - Signal Trading MM Telegram Bot! \n\nYou can use this bot to enter trades directly from Telegram and get a detailed look at your risk to reward ratio with profit, loss, and calculated lot size. You are able to change specific settings such as allowed symbols, risk factor, and more from your personalized Python script and environment variables.\n\nUse the /help command to view instructions and example trades."
     
     # sends messages to user
     update.effective_message.reply_text(welcome_message)
@@ -429,9 +429,9 @@ def help(update: Update, context: CallbackContext) -> None:
 
     help_message = "This bot is used to automatically enter trades onto your MetaTrader account directly from Telegram. To begin, ensure that you are authorized to use this bot by adjusting your Python script or environment variables.\n\nThis bot supports all trade order types (Market Execution, Limit, and Stop)\n\nAfter an extended period away from the bot, please be sure to re-enter the start command to restart the connection to your MetaTrader account."
     commands = "List of commands:\n/start : displays welcome message\n/help : displays list of commands and example trades\n/trade : takes in user inputted trade for parsing and placement\n/calculate : calculates trade information for a user inputted trade"
-    trade_example = "Example Trades 💴:\n\n"
-    market_execution_example = "Market Execution:\n*BUY GBPUSD*\n*Entry NOW*\n*SL 1.14336*\n*TP 1.28930*\n*TP 1.29845*\n\n"
-    limit_example = "Limit Execution:\n*BUY LIMIT GBPUSD*\n*Entry 1.14480*\n*SL 1.14336*\n*TP 1.28930*\n\n"
+    trade_example = "Example Trades:\n\n"
+    market_execution_example = "Market Execution:\nBUY GBPUSD\nEntry NOW\nSL 1.14336\nTP 1.28930\nTP 1.29845\n\n"
+    limit_example = "Limit Execution:\nBUY LIMIT GBPUSD\nEntry 1.14480\nSL 1.14336\nTP 1.28930\n\n"
     note = "You are able to enter up to two take profits. If two are entered, both trades will use half of the position size, and one will use TP1 while the other uses TP2.\n\nNote: Use 'NOW' as the entry to enter a market execution trade."
 
     # sends messages to user
